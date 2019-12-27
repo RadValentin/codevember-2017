2cee9311ddf4a757e2964548288eeb163c20e90c
Valentin Radulescu <radulescu.valentin@gmail.com>
2019-12-27 17:44:26 +0200

    Add Day 5 experiment

---
+++ day-5/README.md
@@ -0,0 +1,9 @@
+I have created over a thousand blades... well more like over 50
+
+<h4>Resources Used</h4>
+<ul>
+<li>Excalibur Fate Stay Night(https://sketchfab.com/models/125699c155bb4507bf430e9a33dc5bd1) by Fopen(https://sketchfab.com/Fopen) is licensed under CC Attribution(http://creativecommons.org/licenses/by/4.0/)</li>
+<li>http://www.virtual-lands-3d.com/textures.html</li>
+<li>http://thecaddshop.net/</li>
+<li>https://free3d.com/3d-model/simple-sword-65623.html</li>
+</ul>
---
+++ day-5/index.js
@@ -0,0 +1,11 @@
+const scene = document.querySelector('a-scene');
+
+for (let i = 0; i < 50; i++) {
+  let sword = document.createElement('a-collada-model');
+  sword.setAttribute('src', '#sword-dae');
+  sword.setAttribute('scale', '0.2 0.2 0.2');
+  sword.setAttribute('position', `${Math.random() * 40 - 20} 1.481 ${(Math.random() * -20) - 3}`);
+  sword.setAttribute('rotation', `${Math.random() * 30 - 90} ${Math.random() * 30} ${Math.random() * 30}`);
+  sword.setAttribute('shadow', '');
+  scene.appendChild(sword);
+}
