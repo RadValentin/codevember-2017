f2063f2c4fa1d82523a8b2881e171c8b60ee54b2
Valentin Radulescu <radulescu.valentin@gmail.com>
2019-12-30 19:54:48 +0200

    Add local assets for Day 5

--- day-5/index.js
+++ day-5/index.js
@@ -1,11 +1,13 @@
 const scene = document.querySelector('a-scene');
 
-for (let i = 0; i < 50; i++) {
-  let sword = document.createElement('a-collada-model');
-  sword.setAttribute('src', '#sword-dae');
-  sword.setAttribute('scale', '0.2 0.2 0.2');
-  sword.setAttribute('position', `${Math.random() * 40 - 20} 1.481 ${(Math.random() * -20) - 3}`);
-  sword.setAttribute('rotation', `${Math.random() * 30 - 90} ${Math.random() * 30} ${Math.random() * 30}`);
-  sword.setAttribute('shadow', '');
-  scene.appendChild(sword);
-}
+scene.addEventListener('loaded', function () {
+  for (let i = 0; i < 50; i++) {
+    let sword = document.createElement('a-collada-model');
+    sword.setAttribute('src', '#sword-dae');
+    sword.setAttribute('scale', '0.2 0.2 0.2');
+    sword.setAttribute('position', `${Math.random() * 40 - 20} 1.481 ${(Math.random() * -20) - 3}`);
+    sword.setAttribute('rotation', `${Math.random() * 30 - 90} ${Math.random() * 30} ${Math.random() * 30}`);
+    sword.setAttribute('shadow', '');
+    scene.appendChild(sword);
+  }
+});
