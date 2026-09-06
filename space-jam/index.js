0e266c30a9db2cf23d017e7d915687b971de4b75
RadValentin <hi@valentin.io>
2026-09-06 19:10:12 +0300

    Restore source files to exactly how they were written initially

--- space-jam/index.js
+++ space-jam/index.js
@@ -1,9 +1,6 @@
-const jam = document.querySelector('.space-jam');
-const video = document.querySelector('#video');
+let jam = document.querySelector('.space-jam');
+let video = document.querySelector('#video');
 
-jam.addEventListener('click', () => {
-  const url = new URL(video.src);
-  url.searchParams.set('autoplay', '1');
-  video.src = url;
+jam.addEventListener('click', e => {
+  video.src += "&autoplay=1"
 });
-
